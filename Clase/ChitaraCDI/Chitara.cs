43181747c67cddb32c532829f93629d433ee1322
Motronu Cozinia <mortonu.cozinia@gmail.com>
2026-03-14 17:30:45 +0200

    Added source code for classes

---
+++ Clase/ChitaraCDI/Chitara.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChitaraCDI
+{
+    class Chitara
+    {
+        //atribute
+        public string nume;
+        public float pret;
+        public bool esteElectrica;
+        private int nrCorzi;
+        protected bool esteBass;
+
+        //constructor deafult(fara parametrii)
+        public Chitara()
+        {
+            this.nume = "Fender";
+            this.pret = (float)800.58;
+            this.esteElectrica = true;
+            this.nrCorzi = 8;
+            this.esteBass = false;
+        }
+
+        // contructor cu parametrii
+        public Chitara(string numeBoss, float catCosta, bool eElectricaDaia)
+        {
+            this.nume = numeBoss;
+            this.pret = catCosta;
+            this.esteElectrica = eElectricaDaia;
+        }
+
+        public int GetNrCorzi()
+        {
+            return this.nrCorzi;
+        }
+
+        public void SetNrCorzi(int NumarulCorzilor)
+        {
+            this.nrCorzi = NumarulCorzilor;
+        }
+
+        public bool GetEsteBass()
+        {
+            return this.esteBass;
+        }
+
+        public void SetEbass(bool Ebass)
+        {
+            this.esteBass = Ebass;
+        }
+
+    }
+}
---
+++ Clase/ChitaraCDI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ChitaraCDI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chitara zdronganitoare = new Chitara();
+            int a = 5;
+            Console.WriteLine("Nume:" + zdronganitoare.nume + "\nPret: " + zdronganitoare.pret + "\nEste electrica? " + zdronganitoare.esteElectrica + "\nNumar corzi: " + zdronganitoare.GetNrCorzi() +"\nEste Bass" + zdronganitoare.GetEsteBass());
+            zdronganitoare.SetEbass(true);
+            zdronganitoare.SetNrCorzi(9);
+            Console.WriteLine("Nume:" + zdronganitoare.nume + "\nPret: " + zdronganitoare.pret + "\nEste electrica? " + zdronganitoare.esteElectrica + "\nNumar corzi: " + zdronganitoare.GetNrCorzi() + "\nEste Bass" + zdronganitoare.GetEsteBass());
+
+
+            Chitara ch2 = new Chitara("Georgica", (float)50.82, false);
+            Console.WriteLine("Nume:" + ch2.nume + "\nPret: " + ch2.pret + "\nEste electrica? " + ch2.esteElectrica);
+            ch2.SetNrCorzi(586);
+            ch2.SetEbass(true);
+            Console.WriteLine("Nume:" + ch2.nume + "\nPret: " + ch2.pret + "\nEste electrica? " + ch2.esteElectrica +"\n nr corzi"+ch2.GetNrCorzi()+"\n estebass"+ch2.GetEsteBass());
+
+
+        }
+    }
+}
